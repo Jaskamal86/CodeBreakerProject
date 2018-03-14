51d1dacdc6f3d2079ccf1fa52ae379f374596a10
Jaskamal86 <37216600+Jaskamal86@users.noreply.github.com>
2018-03-14 14:08:39 +0200

    Update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,11 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+
+    if(answer.value == '' || attempt.value == '')
+    {
+      setHiddenFields();
+    }
 }
 
 function setHiddenFields(){
@@ -12,7 +17,24 @@ function setHiddenFields(){
 
   while(answer.length < 4){
     answer.value += "0";
-    
+
   }
   attempt.value = '0';
+}
+
+function setMessage(message){
+  message.innerHTML = message;
+}
+
+function validateInput(input){
+  if(!input.value){
+    return false;
+  }
+  ateempt.value++;
+}
+
+function getResults(input){
+  let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+
+  
 }

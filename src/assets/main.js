45a6ced942cdd82c7328229c0c00c0bbe5864d53
Jaskamal86 <37216600+Jaskamal86@users.noreply.github.com>
2018-03-14 14:00:51 +0200

    Update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -11,7 +11,7 @@ function setHiddenFields(){
   answer.value = Math.floor(Math.random() * 10000).toString();
 
   while(answer.length < 4){
-    answer += "0";
+    answer.value += "0";
     
   }
   attempt.value = '0';

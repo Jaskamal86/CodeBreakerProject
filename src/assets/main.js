f884a55f23673e7ac9ad36c69f600ba685ca4957
Jaskamal86 <37216600+Jaskamal86@users.noreply.github.com>
2018-03-14 13:59:59 +0200

    Update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,10 +8,11 @@ function guess() {
 
 function setHiddenFields(){
 
-  answer = Math.floor(Math.random() * 10000).toString();
+  answer.value = Math.floor(Math.random() * 10000).toString();
 
   while(answer.length < 4){
     answer += "0";
     
   }
+  attempt.value = '0';
 }
